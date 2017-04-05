ebe027097498e875a8e411eb00c1a5e5cc9e0f43
StreamLab.io <zizo1999988@gmail.com>
2017-04-05 03:26:26 +0200

    clear

--- config/stream_lab.php
+++ config/stream_lab.php
@@ -1,5 +1,5 @@
 <?php
 return[
-    'app_id'=>'2c51b58f-64b7-49a2-94a9-d9833cd5f5aa',
-    'token'=>'aef1478fd2c4ab069feee217ec98361385f10f9259ae7f81dcc7a7096aeb33fb6a06ae9dfb7dddf9900f789dfce6633a9575ff37608af6901e705ba7abbbbd3c'
+    'app_id'=>'',
+    'token'=>''
 ];
